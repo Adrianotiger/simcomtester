15373a988b5c744f3b0790a97cdae865a6cc6c71
Adriano <adrianotiger@hotmail.com>
2024-08-09 11:28:20 +0200

    linefeed added

--- js/info.js
+++ js/info.js
@@ -27,7 +27,7 @@ const TabInfo = new class
 
     setTimeout(()=>{
       _CN("div", {}, [_CN("a", {href:"https://github.com/Adrianotiger/simcomtester", target:"_blank"}, ["Github Project"])], this.divGit);
-      _CN("div", {}, [_CN("i", {}, ["ver 0.42 - 2024", _CN("br"), "© Adriano Petrucci"])], this.divGit);
+      _CN("div", {}, [_CN("i", {}, ["ver 0.43 - 2024", _CN("br"), "© Adriano Petrucci"])], this.divGit);
     }, 500);
     
     let i = window.setInterval(()=>{
@@ -68,6 +68,9 @@ const TabInfo = new class
           else
           {
             SIMSerial.Connect();
+            setTimeout(()=>{
+              SIMSerial.SendData("\r\n\r\n");
+            }, 500);
             setTimeout(()=>{
               this.#InitModule(retries+1);
             }, 1000);
