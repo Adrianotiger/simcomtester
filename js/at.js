3ae0169113ab798c5b97f3999a6e6420f435a165
Adriano <adrianotiger@hotmail.com>
2024-12-17 13:19:50 +0100

    Update at.js

--- js/at.js
+++ js/at.js
@@ -554,7 +554,7 @@ async function addATCommands(jsonFile)
     ATTotScripts += j.length;
     j.forEach(cmd=>{
       cmd = cmd.replace("+", "_");
-      _CN("script", {src:`modules/at/${cmd}.js?v=96`}, [], document.head).addEventListener("load", ()=>{
+      _CN("script", {src:`modules/at/${cmd}.js?v=97`}, [], document.head).addEventListener("load", ()=>{
         ATScriptsLoaded++;
       });
     });
